517863bf1045609c188c97434d73668f83d79f50
Dima Zemnitski <g.fredex@gmail.com>
2024-11-10 02:27:56 +0300

    feat: added sorting by genre

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const BASE_URL = "https://api.rawg.io/api/games"
-const API_KEY = "5a468c92804f440d86a397954555b489"
+const BASE_URL = "https://api.rawg.io/api/games";
+const API_KEY = "5a468c92804f440d86a397954555b489";
+const GENRE_URL = "https://api.rawg.io/api/genres";
 const btnNext = document.getElementById('next');
 const btnBack = document.getElementById('back');
 const searchInput = document.getElementById('search-input');
@@ -7,9 +8,12 @@ const btnSearch = document.getElementById('search-btn');
 const firstPage = document.querySelector('.first-page');
 const lastPage = document.querySelector('.last-page');
 const curPage = document.querySelector('.current-page');
+const selectGenre = document.getElementById('select-genre');
+let selectedGenre = '';
 let currentPage = 1;
-// let
 let strSearch = '';
+
+
 
 function render(games) {
     const wrapGames = document.querySelector('.wrap-games');
@@ -43,11 +47,19 @@ btnBack.addEventListener('click', () => {
     }
 });
 
-async function fetchAllGames() {
-    const response = await fetch(`${BASE_URL}?key=${API_KEY}`);
+async function fetchGenresGames() {
+    const response = await fetch(`${GENRE_URL}?key=${API_KEY}`);
     const data = await response.json();
     return data;
 }
+async function loadSelect() {
+    let dataGenres = await fetchGenresGames();
+    dataGenres.results.forEach(genre => {
+        let newOption = new Option(`${genre.name}`, `${genre.name}`);
+        selectGenre.appendChild(newOption);
+    });
+}
+loadSelect();
 
 async function fetchGames(page) {
     const response = await fetch(`${BASE_URL}?key=${API_KEY}&page=${page}`);
@@ -55,21 +67,43 @@ async function fetchGames(page) {
     return data;
 }
 
-async function searchGame(search, page = 1) {
-    const res = await fetch(`${BASE_URL}?key=${API_KEY}&page=${page}&search=${search}`);
+async function searchGame(search, page = 1, genres = '') {
+    let res;
+    if (genres != '') {
+        res = await fetch(`${BASE_URL}?key=${API_KEY}&page=${page}&genres=${genres}&search=${search}`);
+    } else {
+        res = await fetch(`${BASE_URL}?key=${API_KEY}&page=${page}&search=${search}`);
+    }
+
     const data = await res.json();
+    lastPage.textContent = Math.ceil(data.count / 20);
+    return data;
+}
+
+async function loadGamesGenre(genre, page = 1) {
+    const res = await fetch(`${BASE_URL}?key=${API_KEY}&page=${page}&genres=${genre}`);
+    const data = await res.json();
+    lastPage.textContent = Math.ceil(data.count / 20);
+    console.log(data);
+    console.log(`${BASE_URL}?key=${API_KEY}&genres=${genre}`);
+
+
     return data;
 }
 
 async function loadGames() {
     let dataPage;
-    if (strSearch) {
+    if (strSearch != '') {
         dataPage = await searchGame(strSearch, currentPage);
+    } else if (selectedGenre != '' && strSearch != '') {
+        dataPage = await searchGame(strSearch, currentPage, selectedGenre);
+    } else if (selectedGenre != '') {
+        dataPage = await loadGamesGenre(selectedGenre, currentPage);
+        console.log('вызываю LoadGamesGenre');
     } else {
         dataPage = await fetchGames(currentPage);
     }
     lastPage.textContent = Math.ceil(dataPage.count / 20);
-    console.log(lastPage);
     firstPage.textContent = 1;
     curPage.textContent = currentPage;
     render(dataPage.results);
@@ -80,12 +114,14 @@ async function loadGames() {
 btnSearch.addEventListener('click', async () => {
     strSearch = searchInput.value.trim();
     const dataSearch = await searchGame(strSearch);
-    console.log(searchGame(searchInput.value.trim()));
-
     render(dataSearch.results);
-    console.log(dataSearch);
-
     searchInput.value = '';
+});
+
+selectGenre.addEventListener('change', (e) => {
+    selectedGenre = e.target.value.toLowerCase();
+    console.log(selectedGenre);
+    loadGames();
 });
 
 lastPage.addEventListener('click', (e) => {
@@ -97,5 +133,6 @@ firstPage.addEventListener('click', () => {
     currentPage = 1;
     loadGames();
 });
+
 
 loadGames();
